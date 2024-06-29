86a20b9212042a2d8ca2fb9e73b440baa093c2df
Callum B <callum.berry76@gmail.com>
2024-06-29 20:47:44 +1000

    worked on pairs trading strategy

--- Algothon 2024/teamName.py
+++ Algothon 2024/teamName.py
@@ -27,7 +27,7 @@ def getMyPosition(prcSoFar):
 
     #     if p_value < 0.05:
     #         stationary_instruments.append(i)
-
+    mean_reversion_adjustment = 50
     window_size = 250
     for i in stationary_instruments:
         rolling_window = prcSoFar[i, -window_size:]
@@ -38,12 +38,52 @@ def getMyPosition(prcSoFar):
         if prcSoFar[i, -1] < moving_average - 2 * moving_std_dev:
             if newPos[i] < 0:
                 newPos[i] = 0
-            newPos[i] += 6
+            newPos[i] += mean_reversion_adjustment
         elif prcSoFar[i, -1] > moving_average + 2 * moving_std_dev:
             if newPos[i] > 0:
                 newPos[i] = 0
-            newPos[i] -= 6
-        
+            newPos[i] -= mean_reversion_adjustment
+    
+    instrument_pairs = [27, 38]
+    pair_adjustment = 50 # ?? can this be optimised
+    returns = np.diff(prcSoFar, axis=1) / prcSoFar[:, :-1] * 100
+    for i in instrument_pairs:
+        if (nt < 300): 
+            continue
+        for j in instrument_pairs:
+            if j <= i:
+                continue
+            instrument1 = i
+            instrument2 = j
+
+            moving_average1 = np.mean(returns[instrument1, :])
+            moving_average2 = np.mean(returns[instrument2, :])
+
+            spread = returns[instrument1, -1] - returns[instrument2, -1]
+            spread_moving_average = moving_average1 - moving_average2
+            spread_std_dev = np.std(returns[instrument1, :] - returns[instrument2, :])
+
+            if spread < (spread_moving_average - 2 * spread_std_dev):
+                # Instrument 1 is undervalued, Instrument 2 is overvalued
+                if newPos[instrument1] <= 0:
+                    newPos[instrument1] = 0
+                if newPos[instrument2] >= 0:
+                    newPos[instrument2] = 0
+                newPos[instrument1] += pair_adjustment
+                newPos[instrument2] -= pair_adjustment
+            elif spread > (spread_moving_average + 2 * spread_std_dev):
+                # Instrument 1 is overvalued, Instrument 2 is undervalued
+                if newPos[instrument1] >= 0:
+                    newPos[instrument1] = 0
+                if newPos[instrument2] <= 0:
+                    newPos[instrument2] = 0
+                newPos[instrument1] -= pair_adjustment
+                newPos[instrument2] += pair_adjustment
+            elif spread_moving_average - spread_std_dev <= spread <= spread_moving_average + spread_std_dev:
+                # Close positions when spread reverts to within one standard deviation of the mean spread
+                newPos[instrument1] = 0
+                newPos[instrument2] = 0
+
     currentPos = newPos
     return newPos
 
